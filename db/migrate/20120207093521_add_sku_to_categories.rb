e9f9b6f11513118ef08dd210be9552a1cbc9661a
Jason Knight <jason@jason-knight-martin.com>
2012-02-11 20:57:16 +0100

    added rescue on add_column for add_skus

--- db/migrate/20120207093521_add_sku_to_categories.rb
+++ db/migrate/20120207093521_add_sku_to_categories.rb
@@ -1,6 +1,10 @@
 class AddSkuToCategories < ActiveRecord::Migration
   def change
+    begin
     add_column :categories, :sku, :string
+    rescue
+      puts $!.inspect
+    end
     Category.all.each do |cat|
       if cat.sku.blank? and not cat.vendor.nil? then
         cat.update_attribute :sku, "#{cat.vendor.name}:#{cat.name}".gsub(/[^a-zA-Z0-9]+/,'')
