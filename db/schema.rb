496f88dd79ead4c9ab2b8c27471ad276c3596219
scottie <scottie@scottie-ubuntu>
2012-02-24 12:01:25 +0100

    Added onscreen keyboard to items/selection

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120212140238) do
+ActiveRecord::Schema.define(:version => 20120221113956) do
 
   create_table "actions", :force => true do |t|
     t.string   "name"
@@ -50,12 +50,12 @@ ActiveRecord::Schema.define(:version => 20120212140238) do
   create_table "buttons", :force => true do |t|
     t.string   "name"
     t.string   "sku"
-    t.string   "category"
+    t.string   "old_category_name"
     t.integer  "position"
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
     t.integer  "vendor_id"
-    t.boolean  "is_buyback",  :default => false
+    t.boolean  "is_buyback",        :default => false
     t.integer  "category_id"
     t.string   "color"
   end
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(:version => 20120212140238) do
     t.integer  "vendor_id"
     t.string   "scale"
     t.boolean  "hidden",                :default => false
-    t.boolean  "artema_hybrid",         :default => false
+    t.boolean  "paylife",               :default => false
     t.string   "bank_machine_path"
     t.string   "cash_drawer_path"
     t.boolean  "big_buttons",           :default => false
