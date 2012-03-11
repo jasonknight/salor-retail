8de33852af671ef6102e396cbfde19959317ba6b
scottie <scottie@scottie-ubuntu>
2012-03-11 12:51:38 +0100

    Note and Tag are reset when Cash Drop is finished

--- db/schema.rb
+++ db/schema.rb
@@ -53,13 +53,14 @@ ActiveRecord::Schema.define(:version => 20120305163324) do
     t.string   "name"
     t.string   "sku"
     t.string   "old_category_name"
-    t.integer  "position"
+    t.integer  "weight"
     t.datetime "created_at",                           :null => false
     t.datetime "updated_at",                           :null => false
     t.integer  "vendor_id"
     t.boolean  "is_buyback",        :default => false
     t.integer  "category_id"
     t.string   "color"
+    t.integer  "position"
     t.integer  "hidden",            :default => 0
   end
 
