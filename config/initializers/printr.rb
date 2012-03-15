1a126d0fa78a00f5cc5df3ec86297f97958afefc
Michael Franzl <office@michaelfranzl.com>
2012-03-15 16:44:32 +0100

    fix

--- config/initializers/printr.rb
+++ config/initializers/printr.rb
@@ -1,9 +1,9 @@
 Printr.setup do |config|
-  #config.printr_source = {:active_record => {
-  #  :class_name => VendorPrinter,
-  #  :name => :name,
-  #  :path => :path
-  #}}
+  config.printr_source = {:active_record => {
+    :class_name => VendorPrinter,
+    :name => :name,
+    :path => :path
+  }}
   #config.sanitize_tokens = ['$','\$']
   config.debug = false
   config.sanitize = true
