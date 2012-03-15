c96d3302c6915d29d08f6c43353a9fe5fa02e498
Michael Franzl <office@michaelfranzl.com>
2012-03-15 16:32:10 +0100

    printr encoding

--- config/initializers/printr.rb
+++ config/initializers/printr.rb
@@ -1,10 +1,12 @@
 Printr.setup do |config|
-  config.printr_source = {:active_record => {
-    :class_name => VendorPrinter,
-    :name => :name,
-    :path => :path
-  }}
+  #config.printr_source = {:active_record => {
+  #  :class_name => VendorPrinter,
+  #  :name => :name,
+  #  :path => :path
+  #}}
   #config.sanitize_tokens = ['$','\$']
   config.debug = false
+  config.sanitize = true
+  config.encoding = 'ISO-8859-15'
   config.logger = File.open("/tmp/printr.txt",'a')
 end
