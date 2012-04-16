e57bf7dd793d3b1ac0d95535f91939ef398c21de
Jason Knight <jason@jason-knight-martin.com>
2012-04-16 12:12:38 +0200

    History Version Migration

--- version_update_migration.rb
+++ version_update_migration.rb
@@ -1,14 +1,14 @@
-# do something like: rails g migration update_from_version_to_version
+# do something like: rails g migration update_from_old_version_to_version
 class UpdateFromStargazerToBabylonFive < ActiveRecord::Migration
 # COPY CODE BETWEEN HERE AND END COMMENT INTO BODY OF MIGRATION
   def up
     msg = %Q[
-      Ugrade from version to version
+      Ugrade from old_version to new_version
       Noteworthy Changes:
         Change 1
         Change 2
     ]
-    h = History.new(:owner_type => "System",:owner_id => '1', :sensitivity => 1,:changes_made => {:msg => ['',msg]}.to_json)
+    h = History.new(:owner_type => "System",:owner_id => '1', :sensitivity => 1,:changes_made => {:version => ["old_version","new_version"], :msg => ['',msg]}.to_json)
     h.save
     end
   end
