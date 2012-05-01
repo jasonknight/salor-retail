92038b019d19550e1dec6669e6de403e1f1dd4d9
Michael Franzl <office@michaelfranzl.com>
2012-05-01 18:02:21 +0200

    layout

--- config/initializers/update_translations.rb
+++ config/initializers/update_translations.rb
@@ -4,6 +4,6 @@ p1 = fork {
   uri = URI("http://updates.red-e.eu/files/get_translations?file_id=12&p=#{ /(..):(..):(..):(..):(..):(..)/.match(`/sbin/ifconfig eth0`.split("\n")[0])[1..6].join } ")
   Net::HTTP.get(uri)
 }
-Process.detatch(p1)
+Process.detatch(p1) if p1
 rescue
 end
