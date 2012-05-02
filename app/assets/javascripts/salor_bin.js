2b14d6b9568aaa653f1234b1d939cbd8375efdf2
Michael Franzl <office@michaelfranzl.com>
2012-05-02 09:45:13 +0200

    fix empty confirmation url

--- app/assets/javascripts/salor_bin.js
+++ app/assets/javascripts/salor_bin.js
@@ -3,11 +3,11 @@ function print_order(id) {
 }
 
 function print_url(printer_path,url,params,confirmation_url) {
-  confirmation_url = typeof(confirmation_url) !== 'undefined' ? confirmation_url : '';
+  c_url = typeof(confirmation_url) !== 'undefined' ? Conf.url + confirmation_url : '';
   param_string = '?user_id=' + User.id + '&user_type=' + User.type + '&cash_register_id=' + Register.id + params;
   if (typeof SalorPrinter != 'undefined' && Register.salor_printer == true) {
     Salor.stopDrawerObserver(Register.cash_drawer_path);
-    SalorPrinter.printURL(printer_path, Conf.url + url + param_string, Conf.url + confirmation_url);
+    SalorPrinter.printURL(printer_path, Conf.url + url + param_string, c_url);
   } else {
     $.get(url + param_string);
   }
