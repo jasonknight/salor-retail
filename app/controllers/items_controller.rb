5fe457ea30838e94174ce2c27f08bb9ee47d2615
Michael <michael@michael-N50Vn.(none)>
2012-07-10 22:18:46 +0200

    bug #1219

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,7 +43,7 @@ class ItemsController < ApplicationController
     @from, @to = assign_from_to(params)
     @from = @from ? @from.beginning_of_day : 1.month.ago.beginning_of_day
     @to = @to ? @to.end_of_day : DateTime.now
-    @sold_times = OrderItem.scopied.find(:all, :conditions => {:sku => @item.sku, :hidden => 0, :is_buyback => false, :refunded => false, :created_at => @from..@to}).size
+    @sold_times = OrderItem.scopied.find(:all, :conditions => {:sku => @item.sku, :hidden => 0, :is_buyback => false, :refunded => false, :created_at => @from..@to}).collect{ |i| i.quantity }.sum
   end
 
   # GET /items/new
