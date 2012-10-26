f62f8b89d25a4bd61c947bef507f3913280b7356
Michael Franzl <office@michaelfranzl.com>
2012-10-26 13:30:17 +0200

    cleaned up and restructured for debian

--- app/helpers/actions_helper.rb
+++
@@ -1,2 +0,0 @@
-module ActionsHelper
-end
--- nodes_test.sql
+++
@@ -1,6 +0,0 @@
-delete from salor1_production.cues where 1;
-delete from salor_development.cues where 1;
-delete from salor_development.nodes where is_self IS FALSE;
-delete from salor1_production.nodes where is_self IS FALSE;
-delete from salor1_production.node_messages where 1;
-delete from salor_development.node_messages where 1;
