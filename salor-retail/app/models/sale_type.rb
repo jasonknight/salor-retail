3153838d796e4df68b025d0d121714d3883cc33e
Jason Knight <jason@jason-knight-martin.com>
2012-11-01 13:14:01 +0100

    More intl. fixing forms, and setting up JS variables for config popup

--- salor-retail/app/models/sale_type.rb
+++ salor-retail/app/models/sale_type.rb
@@ -2,4 +2,7 @@ class SaleType < ActiveRecord::Base
   include SalorScope
   include SalorBase
   include SalorModel
+  def as_json(x)
+    return {:id=> self.id, :name=> self.name,:vendor_id=>self.vendor_id}
+  end
 end
