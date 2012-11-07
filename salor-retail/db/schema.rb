73f9a11a089d92a897e78e604e3e35aa34523e88
Michael Franzl <office@michaelfranzl.com>
2012-11-07 17:39:35 +0100

    added missing plusminus migration

--- salor-retail/db/schema.rb
+++ salor-retail/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106171547) do
+ActiveRecord::Schema.define(:version => 20121107163908) do
 
   create_table "actions", :force => true do |t|
     t.string   "name"
