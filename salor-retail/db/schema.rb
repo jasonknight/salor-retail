dbf0b660c6e524a53b3d314e9c996248519fc47e
Jason Knight <jason@jason-knight-martin.com>
2012-11-13 22:20:59 +0100

    post conflict with schema.rb

--- salor-retail/db/schema.rb
+++ salor-retail/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121113182444) do
+ActiveRecord::Schema.define(:version => 20121113192954) do
 
   create_table "actions", :force => true do |t|
     t.string   "name"
