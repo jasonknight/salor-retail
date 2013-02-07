366158fc80f97f145d30400f827b263a3f702fe8
Jason Knight <jason@jason-knight-martin.com>
2013-02-07 12:04:41 -0500

    latest

--- salor-retail/app/assets/javascripts/employees.js
+++ salor-retail/app/assets/javascripts/employees.js
@@ -1,6 +1,6 @@
 $(function () {
-  var lin = $('#employee_login_time');
-  var lout = $('#employee_logout_time');
+  var lin = $('.employee_login_time');
+  var lout = $('.employee_logout_time');
   lin.datetimepicker();
   lout.datetimepicker();
 });
