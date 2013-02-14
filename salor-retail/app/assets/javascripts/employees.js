d5e9b8afdaf98bb69990d5dbacaa0a7977e703c1
Jason Knight <jason@jason-knight-martin.com>
2013-02-14 15:59:35 -0500

    Edit Employee Logins

--- salor-retail/app/assets/javascripts/employees.js
+++ salor-retail/app/assets/javascripts/employees.js
@@ -1,30 +1,38 @@
 $(function () {
-  var lin = $('.employee_login_time');
-  var lout = $('.employee_logout_time');
-  lin.datetimepicker(
-    {
-      timeFormat: 'HH:mm:ss',
-      dateFormat: 'yy/mm/dd',
-      onSelect: function (dateTimeText,picker) {
-        var string = '/vendors/edit_field_on_child?id=' +
-        $(picker.$input).attr('model_id') +'&klass=EmployeeLogin' +
-        '&field=login'+
-        '&value=' + dateTimeText;
-        $.get(string);
+  if (User.role_cache.indexOf('manager') != -1) {
+    var lin = $('.employee_login_time');
+    var lout = $('.employee_logout_time');
+    lin.datetimepicker(
+      {
+        timeFormat: 'HH:mm:ss',
+        dateFormat: 'yy/mm/dd',
+        onSelect: function (dateTimeText,picker) {
+
+          var string = '/vendors/edit_field_on_child?id=' +
+          $(picker.$input).attr('model_id') +'&klass=EmployeeLogin' +
+          '&field=login'+
+          '&value=' + dateTimeText;
+          $.get(string);
+        }
       }
-    }
-  );
-  lout.datetimepicker(
-    {
-      timeFormat: 'HH:mm:ss',
-      dateFormat: 'yy/mm/dd',
-      onSelect: function (dateTimeText,picker) {
-        var string = '/vendors/edit_field_on_child?id=' +
-        $(picker.$input).attr('model_id') +'&klass=EmployeeLogin' +
-        '&field=logout'+
-        '&value=' + dateTimeText;
-        $.get(string);
+    );
+    lout.datetimepicker(
+      {
+        timeFormat: 'HH:mm:ss',
+        dateFormat: 'yy/mm/dd',
+        onSelect: function (dateTimeText,picker) {
+          
+          var string = '/vendors/edit_field_on_child?id=' +
+          $(picker.$input).attr('model_id') +'&klass=EmployeeLogin' +
+          '&field=logout'+
+          '&value=' + dateTimeText;
+          $.get(string);
+        }
       }
-    }
-  );
+    );
+  } // if user.role_cache
 });
+
+function remove_employee_login(thetd) {
+  var thetr = $(thetd).closest("tr")
+}
