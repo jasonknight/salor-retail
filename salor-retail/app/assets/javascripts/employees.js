c677da38ae81a977f9604320469f3b3b1bff92a4
Jason Knight <jason@jason-knight-martin.com>
2013-02-23 06:50:11 -0500

    Fixing employees error

--- salor-retail/app/assets/javascripts/employees.js
+++ salor-retail/app/assets/javascripts/employees.js
@@ -1,35 +1,37 @@
 $(function () {
   if (User.role_cache.indexOf('manager') != -1) {
-    var lin = $('.employee_login_time');
-    var lout = $('.employee_logout_time');
-    lin.datetimepicker(
-      {
-        timeFormat: 'HH:mm:ss',
-        dateFormat: 'yy-mm-dd',
-        onSelect: function (dateTimeText,picker) {
-
-          var string = '/vendors/edit_field_on_child?id=' +
-          $(picker.$input).attr('model_id') +'&klass=EmployeeLogin' +
-          '&field=login'+
-          '&value=' + dateTimeText;
-          $.get(string);
+    try {
+      var lin = $('.employee_login_time');
+      var lout = $('.employee_logout_time');
+      lin.datetimepicker(
+        {
+          timeFormat: 'HH:mm:ss',
+          dateFormat: 'yy-mm-dd',
+          onSelect: function (dateTimeText,picker) {
+            
+            var string = '/vendors/edit_field_on_child?id=' +
+            $(picker.$input).attr('model_id') +'&klass=EmployeeLogin' +
+            '&field=login'+
+            '&value=' + dateTimeText;
+            $.get(string);
+          }
         }
-      }
-    );
-    lout.datetimepicker(
-      {
-        timeFormat: 'HH:mm:ss',
-        dateFormat: 'yy-mm-dd',
-        onSelect: function (dateTimeText,picker) {
-          
-          var string = '/vendors/edit_field_on_child?id=' +
-          $(picker.$input).attr('model_id') +'&klass=EmployeeLogin' +
-          '&field=logout'+
-          '&value=' + dateTimeText;
-          $.get(string);
-        }
-      }
-    );
+            );
+            lout.datetimepicker(
+              {
+                timeFormat: 'HH:mm:ss',
+                dateFormat: 'yy-mm-dd',
+                onSelect: function (dateTimeText,picker) {
+                  
+                  var string = '/vendors/edit_field_on_child?id=' +
+                  $(picker.$input).attr('model_id') +'&klass=EmployeeLogin' +
+                  '&field=logout'+
+                  '&value=' + dateTimeText;
+                  $.get(string);
+                }
+              }
+      );
+    } catch (e) { var e = '';}
   } // if user.role_cache
 });
 
