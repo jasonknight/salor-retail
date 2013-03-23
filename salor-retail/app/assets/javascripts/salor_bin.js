03f33f416e9f93cdf24b9370af3e33628ee67add
Jason Knight <jason@jason-knight-martin.com>
2013-03-23 13:46:32 -0400

    Drawer vs Print

--- salor-retail/app/assets/javascripts/salor_bin.js
+++ salor-retail/app/assets/javascripts/salor_bin.js
@@ -1,8 +1,8 @@
-function print_order(id) {
-   print_url(Register.thermal_printer,'/orders/print_receipt', '&order_id=' + id,'/orders/print_confirmed?order_id=' + id);
+function print_order(id,callback) {
+   print_url(Register.thermal_printer,'/orders/print_receipt', '&order_id=' + id,'/orders/print_confirmed?order_id=' + id,callback);
 }
 
-function print_url(printer_path,url,params,confirmation_url) {
+function print_url(printer_path,url,params,confirmation_url, callback) {
   c_url = typeof(confirmation_url) !== 'undefined' ? Conf.url + confirmation_url : '';
   param_string = '?user_id=' + User.id + '&user_type=' + User.type + '&cash_register_id=' + Register.id + params;
   if (params.indexOf('download=true') != -1) {
@@ -11,7 +11,7 @@ function print_url(printer_path,url,params,confirmation_url) {
     Salor.stopDrawerObserver(Register.cash_drawer_path);
     SalorPrinter.printURL(printer_path, Conf.url + url + param_string, c_url);
   } else {
-    $.get(url + param_string);
+    $.get(url + param_string,callback);
   }
 }
 
