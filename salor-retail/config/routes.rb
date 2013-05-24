3d92df13820018bf5902e68ea8d13f5b6414f5f5
Michael Franzl <office@michaelfranzl.com>
2013-05-24 17:39:42 +0200

    cleaned youhavetopay

--- salor-retail/config/routes.rb
+++ salor-retail/config/routes.rb
@@ -144,7 +144,6 @@ SalorRetail::Application.routes.draw do
   match 'employees/login' => 'employees#login'
   match 'home/edit_owner' => 'home#edit_owner'
   match 'home/update_owner' => 'home#update_owner'
-  match 'home/you_have_to_pay' => 'home#you_have_to_pay'
   match 'employees/destroy_login' => 'employees#destroy_login'
   
   
