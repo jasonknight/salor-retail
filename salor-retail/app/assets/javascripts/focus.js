f0ea0d677520c9684319b4d2e4fe322b07da0021
Michael Franzl <office@michaelfranzl.com>
2013-07-10 16:19:15 +0200

    more

--- salor-retail/app/assets/javascripts/focus.js
+++ salor-retail/app/assets/javascripts/focus.js
@@ -10,3 +10,24 @@ function focusInput(inp) {
   inp.addClass('salor-focused');
   inp.focus();
 }
+
+    $("#main_sku_field").attr("disabled", false);
+    focusInput($("#main_sku_field"));
+    $("#main_sku_field").keyup(function(e) {
+      if (e.keyCode == 13) {
+        add_item($("#main_sku_field").val(), '');
+      }
+    })
+    
+      setInterval(function () {
+      if (
+        !$('#cash_drop').is(":visible") && 
+        !$('#complete_order').is(":visible") && 
+        !$('#inplaceedit-div').is(":visible") && 
+        !$('#search').is(":visible") &&
+        !$('.void-order').is(":visible") &&
+        !$('.ui-keyboard').is(":visible") && 
+        !$('.salor-dialog').is(":visible")) {
+      focusInput($("#main_sku_field"));
+    } 
+  }, 2000);
