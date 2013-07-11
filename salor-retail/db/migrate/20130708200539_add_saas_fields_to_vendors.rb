ba03ca2c7dd7689d95bccc3c2aeed377feceb799
Michael Franzl <office@michaelfranzl.com>
2013-07-11 15:18:34 +0200

    more

--- salor-retail/db/migrate/20130708200539_add_saas_fields_to_vendors.rb
+++ salor-retail/db/migrate/20130708200539_add_saas_fields_to_vendors.rb
@@ -1,14 +1,14 @@
 class AddSaasFieldsToVendors < ActiveRecord::Migration
   def change
-#     add_column :vendors, :identifier, :string
-#     add_column :vendors, :full_subdomain, :string
-#     add_column :vendors, :full_url, :string
-#     add_column :vendors, :virtualhost_filter, :string
-#     add_column :vendors, :auth_https_mode, :integer
-#     add_column :vendors, :https, :boolean
-#     add_column :vendors, :auth, :boolean
-#     add_column :vendors, :domain, :string
-#     add_column :vendors, :subdomain, :string
+    add_column :vendors, :identifier, :string
+    add_column :vendors, :full_subdomain, :string
+    add_column :vendors, :full_url, :string
+    add_column :vendors, :virtualhost_filter, :string
+    add_column :vendors, :auth_https_mode, :integer
+    add_column :vendors, :https, :boolean
+    add_column :vendors, :auth, :boolean
+    add_column :vendors, :domain, :string
+    add_column :vendors, :subdomain, :string
     
     Vendor.reset_column_information
     Vendor.all.each do |v|
