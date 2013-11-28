3cca87d29a0aaaf304927530087d5c27cb99f9f1
Michael Franzl <office@michaelfranzl.com>
2013-11-28 10:29:35 +0100

    ipad js fix

--- salor-retail/app/assets/javascripts/items.js
+++ salor-retail/app/assets/javascripts/items.js
@@ -2,10 +2,15 @@ function itemsAddNameTranslation(locale) {
   var tdiv = shared.element('div',{id: 'name_translation_' + locale},'',$('#name_translations'));
   if (_get('existed',tdiv)) {
     return;
-    
+
   }
   var label = shared.element('label',{id: 'name_translation_'+locale+'_label'},locale + ": &nbsp;",tdiv);
-  var inp = shared.element('input',{name: 'item[name_translations]['+locale+']',type: 'text', class:'keyboardable'},'',label);
+  var inp = shared.element('input', {
+    name: 'item[name_translations][' + locale + ']',
+    type: 'text', class:'keyboardable'
+  },
+  '',
+  label);
   inp.css({width: '50%'});
   return inp;
 }
