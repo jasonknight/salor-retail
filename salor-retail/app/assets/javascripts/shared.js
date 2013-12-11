8b796b4ebdf4b56b22a53c48730b74db0ab8181b
Michael Franzl <office@michaelfranzl.com>
2013-12-11 07:00:54 +0100

    working

--- salor-retail/app/assets/javascripts/shared.js
+++ salor-retail/app/assets/javascripts/shared.js
@@ -100,17 +100,6 @@ function div_wrap(text,cls) {
   return '<div class="' + cls + '">'+text+'</div>';
 }
 
-
-
-
-
-
-
-
-
-function displayAdvertising() {
-}
-
 function set_selected(elem,value,type) { /* 0: Match text, 1: match option value*/
   if (value == null) {
     return elem;
@@ -196,27 +185,6 @@ function updateTips( t ) {
 
 
 
-function logout() {
-  $('#logoutform').submit();
-}
-
-/*
- *  Allows us to latch onto events in the UI for adding menu items, i.e. in this case, customers, but later more.
- */
-function emit(msg,packet) {
-  $('body').triggerHandler({type: msg, packet:packet});
-}
-
-function connect(unique_name,msg,fun) {
-  var pcd = _get('plugin_callbacks_done');
-  if (!pcd)
-    pcd = [];
-  if (pcd.indexOf(unique_name) == -1) {
-    $('body').on(msg,fun);
-    pcd.push(unique_name);
-  }
-  _set('plugin_callbacks_done',pcd)
-}
 
 function _get(name,context) {
   if (context) {
