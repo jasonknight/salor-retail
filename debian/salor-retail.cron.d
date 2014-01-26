baf4db9ebdb7c88957a0a6e7175b91e4e5d2c01a
Michael Franzl <office@michaelfranzl.com>
2014-01-26 12:29:58 +0100

    no cronjob for subscription reminder

--- debian/salor-retail.cron.d
+++ debian/salor-retail.cron.d
@@ -3,6 +3,5 @@
 #
 0 2 * * * root test -x /usr/bin/salor-maintenance && /usr/bin/salor-maintenance r
 0 3 * * * root test -x /usr/bin/salor-remote-backup && /usr/bin/salor-remote-backup r
-0 4 * * * root test -x /usr/bin/salor-shippers-import && /usr/bin/salor-shippers-import
-0 4 * * * root test -x /usr/bin/salor-subscription-reminder && /usr/bin/salor-subscription-reminder
-0 5 * * * root test -x /usr/bin/salor-diagnostics && /usr/bin/salor-diagnostics r
+0 4 * * * root test -x /usr/bin/salor-diagnostics && /usr/bin/salor-diagnostics r
+0 5 * * * root test -x /usr/bin/salor-shippers-import && /usr/bin/salor-shippers-import
